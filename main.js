eec9b566d593a60d4002d0806b8dc400e8119899
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-13 11:29:04 +0200

    added esercizio 6 lezione

--- main.js
+++ main.js
@@ -103,3 +103,22 @@
 
 
 
+// Esercizio 6
+// Rappresenta lo stato attuale di una partita sportiva
+
+// const partita = {
+//     minutaggio: 60,
+//     squadraCasa: "Italia",
+//     puntiCasa: 0,
+//     squadraOspite: "Brasile",
+//     puntiOspite: 2,
+//     vantaggio() {
+//         if (this.puntiCasa > this.puntiOspite) {
+//             return "L'Italia é al momento in vantaggio";
+//         } else if (this.puntiCasa === this.puntiOspite) {
+//             return "Al momento le squadre sono pari"
+//         } else {
+//             return "Il Brasile é al momento in vantaggio";
+//         }
+//     }
+// }
