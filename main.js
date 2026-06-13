895235c51deb557c29cfb7c7fdc0f0aa3ef52918
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-13 09:59:02 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,50 @@
+// Esercizio 1
+// Descrivi le proprietà geometriche di un triangolo
+
+// const triangle = {
+//     lati: [
+//         5, 6, 7
+//     ],
+//     tipologia: "scaleno",
+//     figura: "piana"
+// }
+
+
+
+// Esercizio 2
+// Rappresenta i dati anagrafici di una persona fisica
+
+// const person = {
+//     nome: "Patricia Ann",
+//     cognome: "Comendador",
+//     cittadinanza: "Italiana",
+//     residenza: {
+//         indirizzo: "Via Unità d'italia",
+//         numero: 8,
+//         citta: "Verona",
+//         cap: 37131,
+//         provincia: "VR",
+//     },
+//     lavoro: "Programmatore",
+//     dataNascita: 10052001,
+//     luogoNascita: "Filippine"
+// }
+
+
+
+// Esercizio 3
+// Definisci le informazioni essenziali di un libro
+
+// const best_book = {
+//     titolo: "L'isola degli enigmi",
+//     autore: "Loris Barbiero",
+//     annoPubblicazione: 2024,
+//     generi: [
+//         "mystery", "enigmi", "rompicapo"
+//     ],
+//     target: "Young",
+//     pages: 214,
+//     yearsOld() {
+//         return new Date().getFullYear() - this.annoPubblicazione
+//     }
+// }
