0927f37eca4989a398fd9c50b1aba756fdd5d3a5
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-16 10:38:46 +0200

    added esercizio 3

---
+++ esercizio 3/main.js
@@ -0,0 +1,25 @@
+// Esercizio 3
+// descrivi le specifiche tecniche di un elettrodomestico
+
+const appliances = {
+    caratteristicheFisiche: {
+        dimensioni:
+            [
+                "altezza", "larghezza", "profondita"
+            ],
+        peso: 30
+    },
+    prestazioni: {
+        classi: [
+            "energetica", "centrifuga"
+        ],
+        consumi: [
+            "acqua", "energia"
+        ]
+    },
+    funzionalita: [
+        "wifi", "installazione"
+    ]
+}
+
+console.log(appliances);
