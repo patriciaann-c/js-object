72ade269f8fcf355bc69d4a3d42947e84b3574fb
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-17 20:18:07 +0200

    added esercizio 18

---
+++ esercizio 18/main.js
@@ -0,0 +1,27 @@
+// Esercizio 18
+// descrivi la rosa di una squadra di calcio divisa per ruoli
+
+const team = {
+    portieri: {
+        primo: "ottima visione di gioco",
+        secondo: "vice e impegnato nella rotazione",
+        terzo: "in caso di emergenze"
+    },
+    difensori: {
+        centrale: "forti fisicamente",
+        terzino: "giocano sulle fasce",
+        esterno: "difenzori offensivi"
+    },
+    centroCampisit: {
+        centrale: "recupera palla",
+        regista: "forniscono assist agli attaccanti",
+        mezzala: "bravi sia nell'attaccare che nel difendere"
+    },
+    attaccanti: {
+        centravanti: "terminale offensivo",
+        secondaPunta: "giocatore più mobile e tecnico",
+        ala: "attacchi rapidi"
+    }
+}
+
+console.log(team);
