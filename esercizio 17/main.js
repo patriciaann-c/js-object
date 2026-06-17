dfbebc37302d3aee8147fad0b8c7217bb19eb5f2
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-17 20:07:52 +0200

    added esercizio 17

---
+++ esercizio 17/main.js
@@ -0,0 +1,26 @@
+// Esercizio 17
+// descrivi il catalogo di un negozio diviso per categorie e sottocategorie
+
+const catalog = {
+    informatica: {
+        pc: [
+            "pc gaming", "notebook", "pc fissi", "macbook"
+        ],
+        tablet: [
+            "tablet", "cover", "tastiere", "penne digitali"
+        ]
+    },
+    telefonia: {
+        smartphone: [
+            "iphone", "samsung", "vivo", "oppo"
+        ],
+        accessori: [
+            "cuffie", "cover", "powerbank", "caricabatterie"
+        ]
+    },
+    televisori: [
+        "smart tv oled", "smart tv led"
+    ]
+}
+
+console.log(catalog);
