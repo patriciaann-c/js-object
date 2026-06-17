cc4890e807b764f4430dff9b857bf0ae552366d7
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-17 19:58:11 +0200

    added esercizio 15

---
+++ esercizio 15/main.js
@@ -0,0 +1,24 @@
+// Esercizio 15
+// descrivi la lista degli iscritti ad una newsletter
+
+const subscription = {
+    iscritto1: {
+        nome: "Patricia Ann",
+        cognome: "Comendador",
+        email: "patriciaann.c10@gmail.com",
+        dataIscrizione: new Date(),
+        interessi: "laptop",
+        status: "attiva",
+    },
+    iscritto2: {
+        nome: "Athony June",
+        cognome: "Lopez",
+        email: "patriciaann.c10@gmail.com",
+        dataIscrizione: new Date(),
+        interessi: "pc gaming",
+        status: "disiscritto",
+    }
+
+}
+
+console.log(subscription);
