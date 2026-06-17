272476af942a73f7b36f76555d8046100528f8e1
Patricia Ann Comendador <patriciaann.c10@gmail.com>
2026-06-17 18:57:05 +0200

    added esercizio 9

---
+++ esercizio 9/main.js
@@ -0,0 +1,13 @@
+// Esercizio 9
+// descrivi le informazioni di un evento in calendario
+
+const calendar = {
+    titolo: "Compleanno",
+    data: new Date().getDate(),
+    ora: new Date().getHours(),
+    localita: "Via Valeggio",
+    descrizione: "50esimo compleanno",
+    invitati: 200
+}
+
+console.log(calendar);
